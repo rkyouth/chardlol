b59a1ca0080303d8238ebd94682f3872e92de86e
Chard <2444162098@qq.com>
2016-11-23 14:46:29 +0800

    chard

---
+++ chardlol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  chardlol
+//
+//  Created by ChardLl on 2016/11/23.
+//  Copyright © 2016年 com.chard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
